ca7961509536f3b1da0402b4b51c3cacbf10aea1
Charlie Henning <charlie.henning@gmail.com>
2016-11-11 17:23:27 -0600

    updated methods and did some debugging.

--- src/CLI.java
+++ src/CLI.java
@@ -11,6 +11,8 @@ public class CLI {
 	public boolean gameFinished = false;
 	
 	public CLI(int gridSize){
+		//Boolean to quit the game
+		boolean _quit = false;
 		//Initalize the scanner
 		s = new Scanner(System.in);
 		//Set global variable for gridSize
@@ -19,17 +21,24 @@ public class CLI {
 		 logicClass = new Logic();
 		 //Create the game board
 		gameGrid = logicClass.generateGrid(gridSize);
-		//Welcome and rules are displayed
-		System.out.println(welcomeMessage());
 		
-		System.out.println("To begin playing enter 'y', press anything else to see the directions again.");
-		//startGame
-		if(s.next().equals("y")){
-			playGame();
-		}
-		else{
+		do{
+			//Welcome and rules are displayed
 			System.out.println(welcomeMessage());
-		}
+			
+			System.out.println("To begin playing enter 'y', 'q' to quit or anything else to see the directions again.");
+			//startGame
+			String input = s.next();
+			if(input.equalsIgnoreCase("y")){
+				playGame();
+			}
+			else if(input.equalsIgnoreCase("q")){
+				_quit = true;
+			}
+			
+		}while(!_quit);
+		
+		System.exit(0);
 	}
 	
 	public void playGame(){
@@ -51,17 +60,21 @@ public class CLI {
 			//Take in user input
 			try{
 				//Propt user for first tile
-				System.out.println("Please enter the number of first tile you would like to flip:");
+				System.out.println("Please enter the number of the two tiles you would like to flip:");
+				//Get first int
 				choice1 = s.nextInt();
-				//Propt user for second tile
-				System.out.println("Please enter the number of second tile you would like to flip:");
+				//Get second int
 				choice2 = s.nextInt();
+				//Be sure they aren't the same int
 				if(choice1 == choice2){
 					System.out.println("Please enter two different integers");
+					//Repeat loop if values are bad
 					goodInput = false;
 				}
+				//See if values exist in the current set of IDs
 				if(!validSelection(choice1, choice2)){
 					System.out.println("Please enter a value between 1 and " + GridSize*GridSize);
+					//repeat loop if values are bad
 					goodInput = false;
 				}
 			}
@@ -70,37 +83,29 @@ public class CLI {
 				System.out.println("Please enter a valid integer.");
 				goodInput = false;
 			}
+			//If input is good and we have valid  numbers entered proceed to evaluate the numbers
 			if(goodInput && (choice1 != -1 && choice2 != -1)){
+				//Check for match
 				if(!logicClass.evaluateMatch(getTile(choice1), getTile(choice2))){
+					//If they don't match
+					System.out.println("Sorry, it looks like those two don't match!");
+					//Show game board with the wrong guesses as letters
 					dsplyWrongGuessLtrs(choice1, choice2);
+					//time delay
 					delayGame();
+					//clear the screen
 					clearScreen();
 				}
+				//Update the value of game finished
 				gameFinished = logicClass.gameFinished(gameGrid);
-			}
-		
+			}		
+			//If game is not finished repeat loop
 		}while(!gameFinished);
-		
+		System.exit(0);
 	}
 	
 	private boolean validSelection(int choice1, int choice2) {
-		boolean c1Valid = false;
-		boolean c2Valid = false;
-		boolean valid = false;
-		if(choice1 != choice2){
-		for(ArrayList<Tile> ary: gameGrid){
-			for(Tile t : ary){
-				if(t.ID == choice1){
-					c1Valid = true;
-					}
-				if(t.ID == choice2){
-					c2Valid = true;
-				}
-				}
-			}
-			valid = c1Valid && c2Valid;
-		}
-		return valid;
+		return (choice1>= 1 && choice1 <= GridSize*GridSize) && (choice2 >= 1 && choice2 <= GridSize*GridSize);
 	}
 
 	public Tile getTile(int tileID){
@@ -119,7 +124,7 @@ public class CLI {
 	}
 	
 	public String welcomeMessage(){
-		return "Welcome to the Concentration game! \n "
+		return "Welcome to the Concentration game! \n"
 				+ "The rules are as follows: \n"
 				+ "The screen will display a grid of Letters for a short period of time \n"
 				+ "and then the screen will clear and the letters of the grid will be replaced with numbers. \n"
@@ -158,7 +163,7 @@ public class CLI {
 	}
 	
 	public void clearScreen(){
-		for(int i=0; i< 50; i++)
+		for(int i=0; i< 150; i++)
 			System.out.println("");
 	}
 	
