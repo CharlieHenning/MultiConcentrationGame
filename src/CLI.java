1e58dffefbd2eb026601185abacc57b661f4f999
Charlie Henning <charlie.henning@gmail.com>
2016-11-11 11:55:44 -0600

    Removed old files

--- src/CLI.java
+++ src/CLI.java
@@ -1,18 +1,16 @@
-
+import java.util.ArrayList;
 
 public class CLI {
 	public int GridSize;
-
+	public  ArrayList<ArrayList<Tile>> gameGrid;
+	public Logic logicClass;
+	
 	public CLI(int gridSize){
 		this.GridSize = gridSize;
+		 logicClass = new Logic();
+		gameGrid = logicClass.generateGrid(gridSize);
 		
-		//Display Grid
-		//clear screen
-		//Wait for user input
-			//Do input validation
-			//Make move
-			//Return result
-			//Display updated grid
+		
 	}
 	
 }
