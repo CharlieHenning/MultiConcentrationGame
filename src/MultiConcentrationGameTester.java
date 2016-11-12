fd2dabafcd6eb0f335b6f2d86e7d3c7e23e4a565
Charlie Henning <charlie.henning@gmail.com>
2016-11-11 18:49:35 -0600

    Updated the test java file with example tests.

---
+++ src/MultiConcentrationGameTester.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class MultiConcentrationGameTester {
+
+	
+	public static boolean ClassTest(){
+		boolean allTestsPassed = true;
+		//Perform tests here
+		//CLI cli = new CLI(4);
+		//Auto increment test case number
+		int testCaseNumber = 1;
+
+		//Example test case
+		
+		// Test Case 1
+//		if (new BasicTokenizer("", " ", true).countTokens() == 0) {
+//			System.out.println("Test Case " + testCaseNumber + " Passed");
+//		} else {
+//			allTestsPassed = false;
+//			System.out.println("Test Case " + testCaseNumber + " Failed");
+//		}
+//		testCaseNumber++;
+		
+		//Example of how to enter args into the main method of the "Main" class.
+		String[] args = {"-t", "2"};
+		Main.main(args);
+		return allTestsPassed;
+	}
+	public static void main(String[] args) {
+		boolean allTestsPassed = ClassTest();
+
+		if (allTestsPassed) {
+			System.out.println("All tests successfully passed");
+		} else {
+			System.out.println("Some Test(s) failed");
+		}
+	}
+
+}
